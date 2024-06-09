1cc62e14de6b3d61c5e28390f5cab9b1f9ce17b3
AdamStitou <adam.stitou@yahoo.com>
2024-06-09 23:13:06 +0200

    [étape purger le css inutile] Ajout du fichier de configuration pour purger le css (étape suivante executer et minifier les fichier)

---
+++ purgecss.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    content: [
+        'index.php',
+        'topics.php',
+        'team.php',
+        'reset-pwd.php',
+        'posts.php',
+        'polls.php',
+        'message.php',
+        'hub.php',
+        'forum.php',
+        'events.php',
+        'event-page.php',
+        'create-topic.php',
+        'create-poll.php',
+        'create-new-pwd.php',
+        'create-event.php',
+        'create-category.php',
+        'create-blog.php',
+        'contact.php',
+        'categories.php',
+        'blogs.php'
+    ],
+    css: [
+        './css/loader.css',
+        './css/list-page.css',
+        './css/creator-portfolio.min.css',
+        './css/forum-styles.css',
+        './css/inbox.css',
+        './css/flipclock.css',
+        './css/comp-creation.css',
+        './css/contact-util.css',
+        './css/contact-main.css'
+    ],
+    keyframes: true,
+    output: './css/purified',
+    rejected: true,
+    variables: true
+};
